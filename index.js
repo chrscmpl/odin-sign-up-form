60a5318861cb40125d9110d4aec5123a6f78fa3a
chrscmpl <chcampolongo1@gmail.com>
2022-08-24 22:30:10 +0200

    Add custom validations

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const fields = document.getElementsByClassName('field');
+const fields = getFields();
 
-for (field of fields) {
-	// field.addEventListener();
+fields.forEach(field =>
+	field.input.addEventListener('focusout', () => {
+		field.update();
+	})
+);
+
+fields.forEach(field =>
+	field.input.addEventListener(
+		'focus',
+		() =>
+			(field.aggressive =
+				!field.input.checkValidity() && !field.input.validity.valueMissing)
+	)
+);
+
+fields.forEach(field =>
+	field.input.addEventListener('input', () => {
+		if (field.aggressive) field.update();
+	})
+);
+
+document
+	.querySelector("button[type='submit']")
+	.addEventListener('click', () => fields.forEach(field => field.update()));
+
+//prevent the browser from showing default error bubble / hint
+document.addEventListener('invalid', e => e.preventDefault(), true);
+
+function Field(field) {
+	this.field = field;
+	this.input = field.querySelector('input');
+
+	this.update = function () {
+		if (!this.input.checkValidity()) this.showError();
+		else if (this.field.classList.contains('invalid'))
+			this.field.classList.remove('invalid');
+	};
+
+	this.showError = function () {
+		this.field.classList.add('invalid');
+	};
+}
+
+function getFields() {
+	return [...document.getElementsByClassName('field')].map(
+		field => new Field(field)
+	);
 }
